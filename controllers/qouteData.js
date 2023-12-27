13f2b23c78326c12d63485e545e6434c0d1210d4
Parimaru <ptoro@mail.de>
2023-12-28 00:09:33 +0100

    ready for first deployment

--- controllers/qouteData.js
+++ controllers/qouteData.js
@@ -4,7 +4,7 @@ const getAll = async (req, res) => {
   try {
     const allQuoteData = await QuoteData.find({});
     if (!allQuoteData.length) {
-      return res.status(200).json({ msg: 'No data found' });
+      return res.status(404).json({ msg: 'No data found' });
     }
     res.status(200).json(allQuoteData);
   } catch (error) {
@@ -18,11 +18,26 @@ const getOne = async (req, res) => {
     const oneQuoteData = await QuoteData.find({ _id: quote_id });
     console.log(oneQuoteData);
     if (!oneQuoteData.length) {
-      return res.status(200).json({ msg: 'No data found' });
+      return res.status(404).json({ msg: 'No data found' });
     }
     res.status(200).json(oneQuoteData);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+const getTop = async (req, res) => {
+  try {
+    const topQuoteData = await QuoteData.find().sort({ votes: -1 }).limit(10);
+
+    if (!topQuoteData.length) {
+      return res.status(404).json({ msg: 'No data found' });
+    }
+    res.status(200).json(topQuoteData);
+  } catch (error) {
+    res.status(500).json({
+      error,
+    });
   }
 };
 
@@ -35,7 +50,7 @@ const createNew = async (req, res) => {
       emptyFields.push('fakeAuthor');
     }
     if (!creator) {
-      emptyFields.push('fakeAuthor');
+      emptyFields.push('creator');
     }
     if (emptyFields.length > 0) {
       return res.status(400).json({ error: 'Please fill all fields' });
@@ -56,25 +71,36 @@ const createNew = async (req, res) => {
   }
 };
 
-const voteUp = async (req, res) => {
+const vote = async (req, res) => {
   try {
     const { id } = req.params;
-
-    const quoteData = await QuoteData.findByIdAndUpdate(
-      id,
-      {
-        $inc: { funny: 1 },
-      },
-      {
-        new: true,
-      }
-    );
-    if (!quoteData) {
-      res.status(404).json({ msg: "Quote to vote up can't be found " });
+    const { isFunny } = req.body;
+    let quoteData;
+    if (isFunny === true) {
+      quoteData = await QuoteData.findByIdAndUpdate(
+        id,
+        {
+          $inc: { funny: 1 },
+        },
+        {
+          new: true,
+        }
+      );
     } else {
-      res
-        .status(200)
-        .json({ data: quoteData, msg: 'Voted funny successfully' });
+      quoteData = await QuoteData.findByIdAndUpdate(
+        id,
+        {
+          $inc: { notfunny: 1 },
+        },
+        {
+          new: true,
+        }
+      );
+    }
+    if (!quoteData) {
+      res.status(404).json({ msg: "Quote to vote can't be found " });
+    } else {
+      res.status(200).json({ data: quoteData, msg: 'Voted successfully' });
     }
   } catch (error) {
     res.status(500).json({
@@ -83,4 +109,4 @@ const voteUp = async (req, res) => {
   }
 };
 
-module.exports = { createNew, getAll, getOne, voteUp };
+module.exports = { createNew, getAll, getOne, vote, getTop };
--- routes/quoteData.js
+++ routes/quoteData.js
@@ -4,12 +4,14 @@ const {
   getAll,
   createNew,
   getOne,
-  voteUp,
+  vote,
+  getTop,
 } = require('../controllers/qouteData');
 
 const app = express.Router();
 
 app.route('/').get(getAll).post(createNew);
-app.route('/:id').get(getOne).post(voteUp);
+app.route('/top').get(getTop);
+app.route('/:id').get(getOne).put(vote);
 
 module.exports = app;
